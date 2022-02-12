ab59f3ef66fa977b59ac48262277d0859b8c819c
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2022-02-12 19:39:12 -0300

    change files names

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// Importações que lidam com a navegação entre páginas
+// Handle navigation between screens
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Páginas
-import Inicio from './recursos/inicial.js';
-import Entrou from './recursos/entrou.js';
-import Pagina404 from './recursos/qzq.js';
-import Erradas from './recursos/erradas.js';
+// Pages
+import HomeScreen from './pages/home-screen';
+import RightCredentials from './pages/right-credentials';
+import NotFound from './pages/not-found';
+import WrongCredentials from './pages/wrong-credentials';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -17,10 +17,10 @@ class App extends React.Component{
     return(
       <NavigationContainer>
         <Navigator screenOptions={{ headerShown: false }}>
-          <Screen name="Página inicial" component={Inicio} />
-          <Screen name="Login feito!" component={Entrou} />
-          <Screen name="Credenciais erradas" component={Erradas} />
-          <Screen name="Página não existente" component={Pagina404} />
+          <Screen name="HomeScreen" component={HomeScreen} />
+          <Screen name="RightCredentials" component={RightCredentials} />
+          <Screen name="WrongCredentials" component={WrongCredentials} />
+          <Screen name="NotFound" component={NotFound} />
         </Navigator>
       </NavigationContainer>
     )
